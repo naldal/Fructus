069896f44d60bf1726b1cbe70507a36beadde712
naldal <cec7378@gmail.com>
2021-08-15 00:00:06 +0900

    make a detail views for each fruits

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -10,17 +10,25 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTIES
     var fruits: [Fruit] = fruitsData
+    @AppStorage("isOnboarding") var isOnboarding: Bool?
     
     // MARK: - BODY
     var body: some View {
         NavigationView {
             List {
                 ForEach(fruits.shuffled()) { item in
-                    FruitRowView(fruit: item)
-                        .padding(.vertical, 4)
+                    NavigationLink(destination: FruitDetailView(fruit: item)) {
+                        FruitRowView(fruit: item)
+                            .padding(.vertical, 4)
+                    }
                 }
             }
             .navigationTitle("Fruits")
+//            .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local).onEnded({ value in
+//                if value.translation.width > 0 {
+//                    isOnboarding = true
+//                }
+//            }))
         }//: NAVIGATION
     }
 }
---
+++ Fructus/App/SettingsView.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsView.swift
+//  SettingsView
+//
+//  Created by 송하민 on 2021/08/14.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
